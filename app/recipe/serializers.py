8eb7143377e4f001e576f314ce9bd8fdcdd1f5c6
Diego Cascudo <diego.cascudo@travelperk.com>
2020-03-23 17:10:26 +0100

    create recipe

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -38,6 +38,7 @@ class RecipeSerializer(serializers.ModelSerializer):
                   'price', 'link')
         read_only_fields = ('id',)
 
+
 class RecipeDetailSerializer(RecipeSerializer):
     """Serializer for Recipe details"""
     ingredients = IngredientSerializer(many=True, read_only=True)
